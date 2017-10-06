248d386ee8e83811d17e4f8a4ad6ee568b86ad1e
Vipul Vaibhaw <vaibhaw.vipul@gmail.com>
2017-10-06 16:42:15 +0530

    init commit

---
+++ intro_to_deep_learning.py
@@ -0,0 +1,24 @@
+#First Deep Learning Code!
+#Reference Siraj Raval Intro to Deep Learning video 1
+
+import pandas as pd
+from sklearn import linear_model
+import matplotlib.pyplot as plt
+
+#read_data
+
+dataframe = pd.read_fwf("../brain_body.txt")
+print dataframe #print dataframe
+
+x_values = dataframe[["Brain"]]
+y_values = dataframe[["Body"]]
+
+#train model on data
+
+body_reg = linear_model.LinearRegression()
+body_reg.fit(x_values,y_values)
+
+#visualize the data
+plt.scatter(x_values,y_values)
+plt.plot(x_values,body_reg.predict(x_values))
+plt.show()
