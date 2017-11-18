c0f6beea28551f153f95a0c0bf4d725e564cf758
Vipul Vaibhaw <vaibhaw.vipul@gmail.com>
2017-11-19 05:10:14 +0530

    updating the code

--- tensorflow/fashion-mnist/fashion_mnist_tf.py
+++ tensorflow/fashion-mnist/fashion_mnist_tf.py
@@ -1,8 +1,11 @@
-__author__ = "vaibhawvipul"
+from __future__ import absolute_import, division, print_function
+
+#Train Accuracy: 0.998091
+#Test Accuracy: 0.9767
 
 #importing libraries
 import numpy as np
-import matplotlib.pypolot as plt
+import matplotlib.pyplot as plt
 import seaborn as sns
 import tensorflow as tf
 from tensorflow.python.framework import ops
@@ -11,3 +14,203 @@ from tensorflow.examples.tutorials.mnist import input_data
 # Importing fashion mnist
 
 fashion_mnist = input_data.read_data_sets("input/data",one_hot=True)
+
+#shape of training set
+print("Training set  shape: {shape}".format(shape=fashion_mnist.train.images.shape))
+print("Training set  shape: {shape}".format(shape=fashion_mnist.train.labels.shape))
+
+#shape of test set
+
+print("Test set  shape: {shape}".format(shape=fashion_mnist.test.images.shape))
+print("Test set  shape: {shape}".format(shape=fashion_mnist.test.labels.shape))
+
+# Creating dictionary of target classes
+label_dict = {
+ 0: "T-shirt/top",
+ 1: "Trouser",
+ 2: "Pullover",
+ 3: "Dress",
+ 4: "Coat",
+ 5: "Sandal",
+ 6: "Shirt",
+ 7: "Sneaker",
+ 8: "Bag",
+ 9: "Ankle boot"
+}
+
+#print(label_dict)
+
+#Checking out some pictures from datasets
+
+sample1 = fashion_mnist.train.images[2].reshape(28,28)
+# Getting corresponding integer label from one-hot encoded data
+sample_label_1 = np.where(fashion_mnist.train.labels[47] == 1)[0][0]
+
+print("y = {label_index} ({label})".format(label_index=sample_label_1, label=label_dict[sample_label_1]))
+#plt.imshow(sample1, cmap='Greys')
+#plt.show()
+
+# Network parameters
+n_hidden_1 = 128 # Units in first hidden layer
+n_hidden_2 = 128 # Units in second hidden layer
+n_input = 784 # Fashion MNIST data input (img shape: 28*28)
+n_classes = 10
+n_samples = fashion_mnist.train.num_examples # Number of examples in training set
+
+def create_placeholders(n_x, n_y):
+    #n_x -- scalar, size of an image vector (28*28 = 784)
+    #n_y -- scalar, number of classes (10)
+    X = tf.placeholder(tf.float32, [n_x, None], name="X")
+    Y = tf.placeholder(tf.float32, [n_y, None], name="Y")
+    return X, Y
+
+def initialize_parameters():
+    """
+    Initializes parameters to build a neural network with tensorflow. The shapes are:
+                        W1 : [n_hidden_1, n_input]
+                        b1 : [n_hidden_1, 1]
+                        W2 : [n_hidden_2, n_hidden_1]
+                        b2 : [n_hidden_2, 1]
+                        W3 : [n_classes, n_hidden_2]
+                        b3 : [n_classes, 1]
+    """
+    tf.set_random_seed(42)
+    # First hidden layer
+    W1 = tf.get_variable("W1", [n_hidden_1, n_input], initializer=tf.contrib.layers.xavier_initializer(seed=42))
+    b1 = tf.get_variable("b1", [n_hidden_1, 1], initializer=tf.zeros_initializer())
+
+    # Second hidden layer
+    W2 = tf.get_variable("W2", [n_hidden_2, n_hidden_1], initializer=tf.contrib.layers.xavier_initializer(seed=42))
+    b2 = tf.get_variable("b2", [n_hidden_2, 1], initializer=tf.zeros_initializer())
+
+    # Output layer
+    W3 = tf.get_variable("W3", [n_classes, n_hidden_2], initializer=tf.contrib.layers.xavier_initializer(seed=42))
+    b3 = tf.get_variable("b3", [n_classes, 1], initializer=tf.zeros_initializer())
+
+    # Store initializations as a dictionary of parameters
+    parameters = {
+        "W1": W1,
+        "b1": b1,
+        "W2": W2,
+        "b2": b2,
+        "W3": W3,
+        "b3": b3
+    }
+
+    return parameters
+
+def forward_propagation(X, parameters):
+    #LINEAR -> RELU -> LINEAR -> RELU -> LINEAR -> SOFTMA
+    # Retrieve parameters from dictionary
+    W1 = parameters['W1']
+    b1 = parameters['b1']
+    W2 = parameters['W2']
+    b2 = parameters['b2']
+    W3 = parameters['W3']
+    b3 = parameters['b3']
+
+    # Carry out forward propagation
+    Z1 = tf.add(tf.matmul(W1,X), b1)
+    A1 = tf.nn.relu(Z1)
+    Z2 = tf.add(tf.matmul(W2,A1), b2)
+    A2 = tf.nn.relu(Z2)
+    Z3 = tf.add(tf.matmul(W3,A2), b3)
+
+    return Z3
+
+def compute_cost(Z3, Y):
+    # Get logits (predictions) and labels
+    logits = tf.transpose(Z3)
+    labels = tf.transpose(Y)
+
+    # Compute cost
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=labels))
+
+    return cost
+
+def model(train, test, learning_rate=0.0001, num_epochs=16, minibatch_size=32, print_cost=True, graph_filename='costs'):
+    # Ensure that model can be rerun without overwriting tf variables
+    ops.reset_default_graph()
+    # For reproducibility
+    tf.set_random_seed(42)
+    seed = 42
+    # Get input and output shapes
+    (n_x, m) = train.images.T.shape
+    n_y = train.labels.T.shape[0]
+
+    costs = []
+
+    X, Y = create_placeholders(n_x, n_y)
+
+    parameters = initialize_parameters()
+
+    Z3 = forward_propagation(X, parameters)
+
+    cost = compute_cost(Z3, Y)
+
+    #backpropogate to update weight
+    optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cost)
+
+     # Initialize variables
+    init = tf.global_variables_initializer()
+
+    # Start session to compute Tensorflow graph
+    with tf.Session() as sess:
+
+        # Run initialization
+        sess.run(init)
+
+        # Training loop
+        for epoch in range(num_epochs):
+
+            epoch_cost = 0.
+            num_minibatches = int(m / minibatch_size)
+            seed = seed + 1
+
+            for i in range(num_minibatches):
+
+                # Get next batch of training data and labels
+                minibatch_X, minibatch_Y = train.next_batch(minibatch_size)
+
+                # Execute optimizer and cost function
+                _, minibatch_cost = sess.run([optimizer, cost], feed_dict={X: minibatch_X.T, Y: minibatch_Y.T})
+
+                # Update epoch cost
+                epoch_cost += minibatch_cost / num_minibatches
+
+            # Print the cost every epoch
+            if print_cost == True:
+                print("Cost after epoch {epoch_num}: {cost}".format(epoch_num=epoch, cost=epoch_cost))
+                costs.append(epoch_cost)
+
+        # Plot costs
+        plt.figure(figsize=(16,5))
+        plt.plot(np.squeeze(costs), color='#2A688B')
+        plt.xlim(0, num_epochs-1)
+        plt.ylabel("cost")
+        plt.xlabel("iterations")
+        plt.title("learning rate = {rate}".format(rate=learning_rate))
+        plt.savefig(graph_filename, dpi=300)
+        plt.show()
+
+        # Save parameters
+        parameters = sess.run(parameters)
+        print("Parameters have been trained!")
+
+        # Calculate correct predictions
+        correct_prediction = tf.equal(tf.argmax(Z3), tf.argmax(Y))
+
+        # Calculate accuracy on test set
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+
+        print ("Train Accuracy:", accuracy.eval({X: train.images.T, Y: train.labels.T}))
+        print ("Test Accuracy:", accuracy.eval({X: test.images.T, Y: test.labels.T}))
+
+        return parameters
+
+
+# Running our model
+train = fashion_mnist.train
+test = fashion_mnist.test
+
+parameters = model(train, test, learning_rate=0.0005)
